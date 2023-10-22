c6852f87f295f9c90f816cdc03936fe053bb28d7
Charlotte <charlotteleyser@icloud.com>
2023-10-22 16:52:05 +0200

    Add project files.

---
+++ Lab 5 Basic OOP/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_Basic_OOP
+{
+    internal class Circle //Creating circle class
+    {
+        public double Radius { get; set; } //Creating property for radius
+
+        public Circle(double radius) //Constructor for radius to make it a required input
+        {
+            Radius = radius;
+        }
+
+        public double GetArea(double radius) //Method to calculate area and return answer rounded to two decimals
+        {
+            return Math.Round(radius * radius * Math.PI, 2);
+        }
+
+        public double GetVolume(double radius) //Method to calculate volume and return answer rounded to two decimals
+        {
+            return Math.Round(radius * radius * radius * 4 * Math.PI / 3, 2);
+        }
+
+        public double GetCircumference(double radius) //Method to calculate circumference and return answer rounded to two decimals
+        {
+            return Math.Round(radius * 2 * Math.PI, 2);
+        }
+    }
+}
---
+++ Lab 5 Basic OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab_5_Basic_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5); //Create new object with radius 5
+
+            //Prints information on circle's radius and area from method
+            Console.WriteLine($"A circle with the radius {circle1.Radius} has the area {circle1.GetArea(circle1.Radius)}.");
+
+            Circle circle2 = new Circle(6); //Create new object with radius 6
+
+            //Prints information on circle's radius, area, volume and circumference from methods
+            Console.WriteLine($"\nA circle with the radius {circle2.Radius} has:");
+            Console.WriteLine($"Area: {circle2.GetArea(circle2.Radius)}");
+            Console.WriteLine($"Volume: {circle2.GetVolume(circle2.Radius)}");
+            Console.WriteLine($"Circumference: {circle2.GetCircumference(circle2.Radius)}");
+
+            Console.WriteLine("\nAll calculations are rounded to two decimals.");
+        }
+    }
+}
